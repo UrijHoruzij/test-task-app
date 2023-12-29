9923a4ab6a36dce86d2ba7cbf7e868d09da176bd
Юрий Хоружий <urijhoruzij@gmail.com>
2023-12-29 16:03:49 +0300

    replace

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,24 @@ import ParamEditor from './ParamEditor';
 import './App.css';
 
 function App() {
-	console.log(
-		new ParamEditor({
-			params: [
-				{ id: 1, name: 'Назначение', type: 'text' },
-				{ id: 2, name: 'Длина', type: 'text' },
+	const model = new ParamEditor({
+		params: [
+			{ id: 1, name: 'Назначение', type: 'text' },
+			{ id: 2, name: 'Длина', type: 'text' },
+		],
+		model: {
+			paramValues: [
+				{ paramId: 1, value: 'повседневное' },
+				{ paramId: 2, value: 'макси' },
 			],
-			model: {
-				paramValues: [
-					{ paramId: 1, value: 'повседневное' },
-					{ paramId: 2, value: 'макси' },
-				],
-			},
-		}).getModel(),
-	);
+		},
+	}).getModel();
 	return (
 		<div className="App">
 			<ParamEditor
 				params={[
 					{ id: 1, name: 'Назначение', type: 'text' },
 					{ id: 2, name: 'Длина', type: 'text' },
-					{ id: 3, name: 'Длина2', type: 'number' },
 				]}
 				model={{
 					paramValues: [
